999bbae0ff753c1d1ce2dc90265a2b5e6199372a
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-02-02 20:25:20 -0300

    mongo config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
+    mongoose.connect('mongodb+srv://admin:admin@homestaying.wnkaevq.mongodb.net/?retryWrites=true&w=majority',{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
     this.middlewares();
     this.routes();
   }
