0d099ce3fc5307c784efc29631856f98d265d371
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-02-07 02:22:26 -0300

    user creation

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,22 @@
+//methods: index, show, update, store,destroy
+/*
+index:listagem de sessoes
+store:cria sessao
+show:lista unica sessao
+update:atualizar sessao
+destroy: deletar sessao 
+*/
+
+import User from "../models/User";
+
+class SessionController {
+  async store(req, res) {
+    const { email } = req.body;
+
+    let user = await User.create({ email });
+
+    return res.json({ user });
+  }
+}
+
+export default new SessionController();
