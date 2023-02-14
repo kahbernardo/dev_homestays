7062ef2cbe1cf946eafefa6e3ef48d72572fe0e9
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-02-14 04:24:41 -0300

    reserve functions and rules

---
+++ src/controllers/ReserveController.js
@@ -0,0 +1,37 @@
+import Reserve from "../models/Reserve";
+import House from "../models/House";
+import User from "../models/User";
+
+class ReserveController {
+  async store(req, res) {
+    const { user_id } = req.headers;
+    const { house_id } = req.params;
+    const { date } = req.body;
+
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
+
+    if (!houses) {
+      return res.status(400).json({ error: "Casa não encontrada" });
+    }
+    if (houses.status !== true) {
+      return res.status(400).json({ error: "Casa indisponível" });
+    }
+
+    if (String(user._id) == String(houses.user)) {
+      return res.status(401).json({ error: "Reserva não permitida" });
+    }
+
+    const reserve = await Reserve.create({
+      user: user_id,
+      house: house_id,
+      date,
+    });
+
+    await reserve.populate("house").populate("user").execPopulate();
+
+    return res.json(reserve);
+  }
+}
+
+export default new ReserveController();
