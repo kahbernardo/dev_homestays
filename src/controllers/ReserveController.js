3ab372837bd42a9ab5a96f9db629377214bb07a2
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-02-14 04:35:37 -0300

    reserve cancel

--- src/controllers/ReserveController.js
+++ src/controllers/ReserveController.js
@@ -44,6 +44,25 @@ class ReserveController {
 
     return res.json(reserves);
   }
+
+  async destroy(req, res) {
+    const { user_id } = req.headers;
+    const { reserve_id } = req.body;
+
+    const reserves = await Reserve.find({
+      _id: reserve_id,
+    });
+
+    if (String(user_id) !== String(reserves.user)) {
+      return res.status(401).json({ error: "Alteração não permitida" });
+    }
+
+    await Reserve.findByIdAndDelete({
+      _id: reserve_id,
+    });
+
+    return res.status(200).json({ error: "Reserva excluida com sucesso!" });
+  }
 }
 
 export default new ReserveController();
