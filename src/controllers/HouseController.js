26f775d4542621b9590a9b1a8f2ccb15aeb15d3c
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-02-14 03:52:04 -0300

    house methods

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -9,18 +9,76 @@ destroy: deletar sessao
 
 import House from "../models/House";
 
+import User from "../models/User";
+
 class HouseController {
+  async index(req, res) {
+    const { status } = req.query;
+
+    const houses = await House.find({ status });
+
+    return res.json(houses);
+  }
+
   async store(req, res) {
-    console.log("body", req.body);
-    console.log("file", req.file);
-    //   const { email } = req.body;
-    //   let user = await User.findOne({ email });
+    const { filename } = req.file;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
 
-    //   if (!user) {
-    //     user = await User.create({ email });
-    //   }
+    const house = await House.create({
+      user: user_id,
+      thumbnail: filename,
+      description,
+      price,
+      location,
+      status,
+    });
 
-    return res.json({ ok: true });
+    return res.json(house);
+  }
+
+  async update(req, res) {
+    const { filename } = req.file;
+    const { house_id } = req.params;
+    const { description, price, location, status } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
+
+    if (String(user._id) !== String(houses.user)) {
+      return res.status(401).json({ error: "Não autorizado" });
+    }
+
+    await House.updateOne(
+      { _id: house_id },
+      {
+        user: user_id,
+        thumbnail: filename,
+        description,
+        price,
+        location,
+        status,
+      }
+    );
+
+    return res.send();
+  }
+
+  async destroy(req, res) {
+    const { house_id } = req.body;
+    const { user_id } = req.headers;
+
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
+
+    if (String(user._id) !== String(houses.user)) {
+      return res.status(401).json({ error: "Não autorizado" });
+    }
+
+    await House.findByIdAndDelete({ _id: house_id });
+
+    return res.status(200).json({ error: "Casa excluida com sucesso!" });
   }
 }
 
