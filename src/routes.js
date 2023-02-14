790953eb714349d3babf86587c94a958ec6eef11
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-02-14 04:05:03 -0300

    dashboard house show

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import uploadConfig from "./config/upload";
 
 import SessionController from "./controllers/SessionController";
 import HouseController from "./controllers/HouseController";
+import DashboardController from "./controllers/DashboardController";
 
 const routes = new Router();
 const upload = multer(uploadConfig);
 
 routes.post("/sessions", SessionController.store);
+// ------------------------------------------------
 routes.post("/houses", upload.single("thumbnail"), HouseController.store);
 routes.get("/houses", HouseController.index);
 routes.put(
@@ -17,5 +19,7 @@ routes.put(
   HouseController.update
 );
 routes.delete("/houses", HouseController.destroy);
+// ------------------------------------------------
+routes.get("/dashboard", DashboardController.show);
 
 export default routes;
