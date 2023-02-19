060e91efe5ff433a6ca9d8c70c694c3daa7bd098
Kaique Bernardo <kaique@MacBook-Kaique.local>
2023-02-19 03:08:17 -0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "class-methods-use-this": "off",
+    "no-params-reassign": "off",
+    camelcase: "off",
+    "no-underscore-dangle": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
+};
